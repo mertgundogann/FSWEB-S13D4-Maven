6a097a8760eebe37b9a29321bfa4eb0b396481bd
mertgundogann <merttgundogan@hotmail.com>
2025-03-10 05:45:28 +0300

    point bitti.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Point p1 = new Point(3, 4);
+        Point p2 = new Point(6, 8);
+
+        System.out.println("p1'in (0,0) noktasına uzaklığı: " + p1.distance());
+        System.out.println("p1'in p2 noktasına uzaklığı: " + p1.distance(p2));
+        System.out.println("p1'in (1,1) noktasına uzaklığı: " + p1.distance(1, 1));
     }
 }
